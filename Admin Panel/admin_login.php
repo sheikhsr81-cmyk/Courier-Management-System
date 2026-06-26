3eef3071ba164542171cf1bcc5a6522faa6117fe
Sameer Sheikh <sheikhsr81@gmail.com>
2026-06-26 18:42:31 +0500

    Update admin_login.php

--- Admin Panel/admin_login.php
+++ Admin Panel/admin_login.php
@@ -31,8 +31,71 @@ if (isset($_POST['login'])) {
 <head>
     <title>👑Admin Login</title>
 
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: Segoe UI;
+        }
 
-    <link rel="stylesheet" href="../style.css">
+        body {
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: linear-gradient(rgba(15, 23, 42, .9), rgba(15, 23, 42, .9)),
+                url('https://images.unsplash.com/photo-1553413077-190dd305871c?w=1600');
+            background-size: cover;
+            background-position: center;
+        }
+
+        /* LOGIN CARD */
+        .login-box {
+            width: 380px;
+            background: #fff;
+            padding: 35px;
+            border-radius: 15px;
+            box-shadow: 0 10px 30px rgba(0, 0, 0, 0.3);
+            text-align: center;
+        }
+
+        .login-box h2 {
+            margin-bottom: 20px;
+            color: #0f172a;
+        }
+
+        input {
+            width: 100%;
+            padding: 12px;
+            margin: 10px 0;
+            border: 1px solid #ccc;
+            border-radius: 8px;
+            outline: none;
+        }
+
+        button {
+            width: 100%;
+            padding: 12px;
+            border: none;
+            background: #0f172a;
+            color: white;
+            border-radius: 8px;
+            font-size: 16px;
+            cursor: pointer;
+            margin-top: 10px;
+        }
+
+        button:hover {
+            background: #2563eb;
+        }
+
+        .error {
+            color: red;
+            margin-bottom: 10px;
+        }
+    </style>
+
 </head>
 
 <body>
